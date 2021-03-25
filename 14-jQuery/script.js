5a991e2b762e6e7d4916e8c779611fd076126425
Matthijs Boet <m.boet@chello.nl>
2021-03-25 13:50:22 +0100

    learning jQuery

---
+++ 14-jQuery/script.js
@@ -0,0 +1,9 @@
+$("h1").addClass("big-title margin-50");
+$("h1").removeClass("big-title");
+
+$("h1").text("Bye")
+$("button").html("<em>Click</em>");
+
+
+console.log($("h1").hasClass("margin-50"))
+console.log($("button").css("color"))
